4cc8171f18aac03277dfdb800e1c5a0f3815b273
Tehuel <torresbaldi.tehuel@gmail.com>
2020-04-16 09:52:46 -0300

    initializes spring boot using spring initializer https://start.spring.io/

---
+++ src/main/java/com/unq/dapp0/c1/comprandoencasa/ComprandoEnCasaApplication.java
@@ -0,0 +1,13 @@
+package com.unq.dapp0.c1.comprandoencasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprandoEnCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprandoEnCasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/ComprandoEnCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unq.dapp0.c1.comprandoencasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprandoEnCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
