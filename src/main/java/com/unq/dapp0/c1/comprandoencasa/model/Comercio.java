3b1a7774648a060ab75d7d8daedf6be510075827
R4NK <nahumoscatelli@gmail.com>
2020-05-02 19:14:13 -0300

    Agregados tests de Comercio

--- src/main/java/com/unq/dapp0/c1/comprandoencasa/model/Comercio.java
+++ src/main/java/com/unq/dapp0/c1/comprandoencasa/model/Comercio.java
@@ -1,9 +1,23 @@
 package com.unq.dapp0.c1.comprandoencasa.model;
 
-public class Comercio {
-    public Rubro rubro;
+import java.util.ArrayList;
 
-    public Comercio(Rubro rubro) {
-        this.rubro = rubro;
+public class Comercio {
+    private ArrayList<Rubro> rubros;
+
+    public Comercio(ArrayList<Rubro> rubros) {
+        this.rubros = rubros;
+    }
+
+    public ArrayList<Rubro> getRubros() {
+        return this.rubros;
+    }
+
+    public void addRubro(Rubro rubro) {
+        this.rubros.add(rubro);
+    }
+
+    public void removeRubro(Rubro rubro) {
+        this.rubros.remove(rubro);
     }
 }
--- src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ComercioTests.java
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ComercioTests.java
@@ -1,13 +1,55 @@
 package com.unq.dapp0.c1.comprandoencasa.model;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ComercioTests {
+
     @Test
-    public void comercioPoseeElRubroDeAlimentos(){
-        Comercio comercio = new Comercio(Rubro.Alimentos);
-        assertEquals(comercio.rubro, Rubro.Alimentos);
+    public void comercioPoseeElRubroDeAlimentosY(){
+        ArrayList<Rubro> rubros = new ArrayList<>();
+        rubros.add(Rubro.AlimentosYBebidas);
+        rubros.add(Rubro.Libreria);
+        Comercio comercio = new Comercio(rubros);
+
+        assertTrue(comercio.getRubros().contains(Rubro.AlimentosYBebidas));
+        assertTrue(comercio.getRubros().contains(Rubro.Libreria));
+
+        assertFalse(comercio.getRubros().contains(Rubro.ElectronicaYElectrodomesticos));
+        assertFalse(comercio.getRubros().contains(Rubro.Entretenimiento));
+        assertFalse(comercio.getRubros().contains(Rubro.Servicios));
+        assertFalse(comercio.getRubros().contains(Rubro.Hogar));
+        assertFalse(comercio.getRubros().contains(Rubro.AnimalesYMascotas));
+        assertFalse(comercio.getRubros().contains(Rubro.VehiculosYAccesorios));
+        assertFalse(comercio.getRubros().contains(Rubro.Ropa));
+        assertFalse(comercio.getRubros().contains(Rubro.Farmacia));
+    }
+
+    @Test
+    public void comercioPuedeAgregarUnRubroASuListaDeRubros(){
+        ArrayList<Rubro> rubros = new ArrayList<>();
+        rubros.add(Rubro.AlimentosYBebidas);
+        Comercio comercio = new Comercio(rubros);
+
+        assertTrue(comercio.getRubros().contains(Rubro.AlimentosYBebidas));
+        assertFalse(comercio.getRubros().contains(Rubro.Libreria));
+
+        comercio.addRubro(Rubro.Libreria);
+        assertTrue(comercio.getRubros().contains(Rubro.Libreria));
+    }
+
+    @Test
+    public void comercioPuedeQuitarUnRubroDeSuListaDeRubros(){
+        ArrayList<Rubro> rubros = new ArrayList<>();
+        rubros.add(Rubro.AlimentosYBebidas);
+        rubros.add(Rubro.Libreria);
+        Comercio comercio = new Comercio(rubros);
+
+        assertTrue(comercio.getRubros().contains(Rubro.AlimentosYBebidas));
+        assertTrue(comercio.getRubros().contains(Rubro.Libreria));
+
+        comercio.removeRubro(Rubro.Libreria);
+        assertFalse(comercio.getRubros().contains(Rubro.Libreria));
     }
 }
