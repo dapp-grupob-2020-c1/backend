4ded58692f0153d6f2fb7e0a4e99a30cdd3c68f5
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-06 19:37:00 -0300

    test para ProductType

--- src/main/java/com/unq/dapp0/c1/comprandoencasa/model/ProductType.java
+++ src/main/java/com/unq/dapp0/c1/comprandoencasa/model/ProductType.java
@@ -2,4 +2,16 @@ package com.unq.dapp0.c1.comprandoencasa.model;
 
 public class ProductType {
     private String name;
+
+    public ProductType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ProductTypeTest.java
@@ -0,0 +1,21 @@
+package com.unq.dapp0.c1.comprandoencasa.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTypeTest {
+
+    @Test
+    public void aProductTypeCanBeCreatedWithAName(){
+        ProductType prodType = new ProductType("Food");
+        assertEquals("Food", prodType.getName());
+    }
+
+    @Test
+    public void aProductTypeNameCanBeChanged(){
+        ProductType prodType = new ProductType("Food");
+        prodType.setName("Electronics");
+        assertEquals("Electronics", prodType.getName());
+    }
+}
