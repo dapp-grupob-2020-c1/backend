c0d6ab5489cd36b7a77a75e1ce38b347b076679a
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-06 19:37:50 -0300

    refactor: import individuales

--- src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ProductTest.java
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ProductTest.java
@@ -2,7 +2,8 @@ package com.unq.dapp0.c1.comprandoencasa.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class ProductTest {
