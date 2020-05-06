89a4a362d29ca48cc18fc2563fceea73cf28b390
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-06 20:13:14 -0300

    fix
    Error en Codacy: "Fields should be declared at the top of the class, before any method declarations, constructors, initializers or inner classes."

--- src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ProductBuilder.java
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ProductBuilder.java
@@ -3,12 +3,13 @@ package com.unq.dapp0.c1.comprandoencasa.model;
 import java.math.BigDecimal;
 
 public class ProductBuilder {
-    public static ProductBuilder aProduct() {
-        return new ProductBuilder();
-    }
     private String name = "no name";
     private String brand = "no brand";
     private BigDecimal price = new BigDecimal(999);
+
+    public static ProductBuilder aProduct() {
+        return new ProductBuilder();
+    }
 
     public Product build() {
         return new Product(name, brand, price);
