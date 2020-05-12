a81289f0b8c8158eddf720743e8de9805b2e4193
R4NK <nahumoscatelli@gmail.com>
2020-05-12 13:18:40 -0300

    Fix style

--- src/test/java/com/unq/dapp0/c1/comprandoencasa/model/CustomerTests.java
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/model/CustomerTests.java
@@ -2,7 +2,10 @@ package com.unq.dapp0.c1.comprandoencasa.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
