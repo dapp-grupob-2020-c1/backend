8995c03a4d15bfbfbe509baf7e606ef7377225c6
R4NK <nahumoscatelli@gmail.com>
2020-05-12 13:35:51 -0300

    Clean Travis integration

--- src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ManagerTests.java
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/model/ManagerTests.java
@@ -11,7 +11,7 @@ public class ManagerTests {
 
     @Test
     public void aManagerCanBeCreatedWithANameAndAPasswordThatItCanThenValidate(){
-        String name = "Test";
+        String name = "Testo";
         String email = "test@testo.com";
         String password = "1234";
 
