77c29ca0b771bb355df8f04f90f33781398c03d0
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-23 18:01:51 -0300

    agrego notations a ShopDelivery

--- src/main/java/com/unq/dapp0/c1/comprandoencasa/model/ShopDelivery.java
+++ src/main/java/com/unq/dapp0/c1/comprandoencasa/model/ShopDelivery.java
@@ -1,23 +1,43 @@
 package com.unq.dapp0.c1.comprandoencasa.model;
 
-import java.util.ArrayList;
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@Table
 public abstract class ShopDelivery {
+
+    @Id
+    protected Long id;
+
+    @OneToOne
     private final Shop shop;
-    private final ArrayList<Product> products;
+
+    @OneToMany
+    private final List<Product> products;
+
+    @OneToOne
     private final Customer customer;
 
-    public ShopDelivery(Shop shop, ArrayList<Product> products, Customer customer) {
+
+    public ShopDelivery(Shop shop, List<Product> products, Customer customer) {
         this.shop = shop;
         this.products = products;
         this.customer = customer;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Shop getShop(){
         return this.shop;
     }
 
-    public ArrayList<Product> getProducts() {
+    public List<Product> getProducts() {
         return this.products;
     }
 
