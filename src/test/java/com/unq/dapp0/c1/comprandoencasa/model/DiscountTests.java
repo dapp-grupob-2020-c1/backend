6510f4800b055b7e127d805d7647567f7f6257ef
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-29 13:22:33 -0300

    removes unused imports

--- src/test/java/com/unq/dapp0/c1/comprandoencasa/model/DiscountTests.java
+++ src/test/java/com/unq/dapp0/c1/comprandoencasa/model/DiscountTests.java
@@ -6,10 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
